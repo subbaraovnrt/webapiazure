963a77c73c547794c21cf161da44c57587134ff1
SUBBARAO VALISETTY <subbaraovnrt@gmail.com>
2019-02-14 20:25:35 -0600

    adding to branch

---
+++ ConsoleAppSingleTon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleAppSingleTon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var singleTonObj = SingleTon.GetInstance();
+
+            Console.WriteLine($"Increment Value1: {singleTonObj.GetIncrement()}");
+
+            Console.WriteLine($"Increment Value2: {singleTonObj.GetIncrement()}");
+
+            Console.WriteLine($"Increment Value3: {singleTonObj.GetIncrement()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppSingleTon/SingleTon.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleAppSingleTon
+{
+    public class SingleTon
+    {
+        private static SingleTon _singleTonInstance = null;
+
+        private int increment = -1;
+
+        private SingleTon() { }
+
+        public static SingleTon GetInstance()
+        {
+            if (_singleTonInstance != null)
+            {
+                return _singleTonInstance;
+            }
+            else
+            {
+                _singleTonInstance = new SingleTon();
+                return _singleTonInstance;
+            }
+        }
+
+        public int GetIncrement()
+        {
+            return ++increment;
+        }
+    }
+}
